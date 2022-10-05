c6c47bb7b1a669403f94c4f5f4da3c1c0d166d5f
Edel <edelmedina322@gmail.com>
2022-10-05 19:56:23 -0300

    Add counter

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,5 +1,24 @@
+import "../Styles/Counter.css";
+import Finn from "../Images/finn.png";
+import Jake from "../Images/jake.jpg";
+import Marceline from "../Images/marceline.jpg";
+
 const Counter = () => {
-    return;
+    return (
+        <div class="counter">
+            <div class="character-counter">
+                <h1>Remaining characters</h1>
+                <div class="character-display">
+                    <img class="char-portrait" src={Finn} alt="Finn"></img>
+                    <img class="char-portrait" src={Jake} alt="Marceline"></img>
+                    <img class="char-portrait" src={Marceline} alt="Marceline"></img>
+                </div>
+            </div>
+            <div class="clock">
+                Placeholder
+            </div>
+        </div>
+    );
 };
 
 export default Counter;
